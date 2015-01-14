298186b7e7b8e5627bc32352f6218f3e3da3cd57
unknown <1469kirpalsingh@gmail.com>
2015-01-14 14:23:37 +0530

    one

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
 console.log('This would be the main JS file.');
+$(document).ready(function(){
+$("#map").show();
+$("#locateus").hide();
+$("#locateushide").show();
+$("#locateushide").click(function(){
+$("#map").toggle();
+$("#locateus").toggle();
+$("#locateushide").toggle();
+
+  });
+  
+  $("#locateus").click(function(){
+$("#map").toggle();
+$("#locateus").toggle();
+$("#locateushide").toggle();
+
+});
+  
+  
+});
